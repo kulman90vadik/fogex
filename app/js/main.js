c1089c9daeab5b732a97dfb19441e80a164c1a20
Vadim Kulman <vadim.kulman90@mail.ru>
2022-02-18 03:34:59 +0300

    add brands/news/company

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,39 @@ window.addEventListener("load", function(){
 
     // });
 
+    new Swiper('.news-swiper', {
+        wrapperClass: 'news-swiper__slider',
+        slideClass: 'news-swiper__slide',
+        loop: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        slidesPerView: '3',
+        autoplay: {
+            delay: 5000,
+        },
+    });
+
+    const mySwiper = new Swiper('.brands-swiper', {
+        wrapperClass: 'brands-swiper__slider',
+        slideClass: 'brands-swiper__slide',
+        slidesPerView: '6',
+        navigation: {
+            nextEl: '.brands-swiper__arrow--next',
+            prevEl: '.brands-swiper__arrow--prev',
+        },
+    });
+
+    
+
+    // const swiperPrev = document.getElementById('swiperPrev');
+    // const swiperNext = document.getElementById('swiperNext');
+    
+    // swiperPrev.addEventListener('click', () => {
+    //     mySwiper.slidePrev();
+    // });
+    // swiperNext.addEventListener('click', () => {
+    //     mySwiper.slideNext();
+    // });
+
 
 });
